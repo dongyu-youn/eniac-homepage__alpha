e6b00f87215ead91800e53031636c4c6cd6d67dd
LeeSangHee <sang6299@naver.com>
2021-09-17 20:10:24 +0900

    tab fixed

--- js/project-tab.js
+++ js/project-tab.js
@@ -1,23 +1,23 @@
 const tabLink = document.querySelectorAll(".project__tab-menu a");
 const tabContent = document.querySelectorAll(".project__tab-content > div");
 
-for (let i = 0; i < tabLink.length - 1; i++) {
-  // length에 -1해준 이유는 안쓰는 탭 하나 더추가해서임(신경x)
+for (let i = 0; i < tabLink.length; i++) {
   tabLink[i].addEventListener("click", function (e) {
     e.preventDefault(); //링크의 기본 속성 막음(페이지 이동)
 
     const orgTarget = e.target.getAttribute("href"); //클릭된 탭의 속성인 href를 가져옴
     const tabTarget = orgTarget.replace("#", ""); // 그 href에서 #을 없애줌
+    console.log(tabTarget);
 
     for (let j = 0; j < tabLink.length; j++) {
       tabLink[j].classList.remove("active"); //모든 tablink active없애주는데
     }
     e.target.classList.add("active"); //클릭한 tablick는 다시 active시켜줌
 
-    for (let x = 0; x < tabLink.length; x++) {
+    for (let x = 0; x < tabContent.length; x++) {
       tabContent[x].style.display = "none"; //모든 탭 내용들을 안보이게 함
     }
-    document.getElementsByClassName(tabTarget).style.display = "block"; //근데 클릭한 탭은 보이도록 함
+    document.getElementById(tabTarget).style.display = "block"; //근데 클릭한 탭은 보이도록 함
   });
 }
 
