68fdc663562004f898f2347e63ef6ef0a54ddb96
dongyu-youn <donghe1472@naver.com>
2021-09-28 17:58:26 +0900

    new file

--- js/home-box__menu.js
+++ js/home-box__menu.js
@@ -9,3 +9,7 @@ function opentwoNav() {
     document.getElementById("meNav").style.width = "100%";
     home.style.opacity = "0";
 }
+
+function openProfile() {
+    document.getElementsByClassName("sidebar").style.width = "30%";
+}
